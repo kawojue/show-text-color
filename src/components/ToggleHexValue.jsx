1396a2bd737f0aa8eb7a9e8a9a3c75478e7cd31a
Raheem Kawojue <kawojue08@gmail.com>
2022-10-23 10:40:24 +0100

    Edit

--- src/components/ToggleHexValue.jsx
+++ src/components/ToggleHexValue.jsx
@@ -1,6 +1,6 @@
 const ToggleHexValue = ({ toggleHexValue }) => {
     return (
-        <button className="rounded-md px-3 py-0.5 bg-white w-fit" onClick={toggleHexValue}>
+        <button className="rounded-md px-3 py-0.5 bg-white w-fit" onClick={() => toggleHexValue()}>
             Toggle Hex Value
         </button>
     )
