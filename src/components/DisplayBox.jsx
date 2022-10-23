9211d4f061007aa6f30da592e7cddbb7e5cc4925
Raheem Kawojue <kawojue08@gmail.com>
2022-10-23 10:41:12 +0100

    formatted color

--- src/components/DisplayBox.jsx
+++ src/components/DisplayBox.jsx
@@ -1,10 +1,11 @@
-const DisplayBox = ({ inputColor, color, hexValue, toggle }) => {
+const DisplayBox = ({ inputColor, color, hexValue, toggle, displayType }) => {
+    const formatColor = color.trim().split(',').join(',')
     return (
-        <div className="displayBox" style={{ backgroundColor: color }}>
+        <div className="displayBox" style={{ background: `${displayType ? color : `linear-gradient(${formatColor})`}` }}>
             <h1 className="text-xl">{inputColor === "" ? "Empty Value" : inputColor}</h1>
             {toggle && <h1 className="text-lg">{hexValue}</h1>}
         </div>
     )
 }
 
-export default DisplayBox;
+export default DisplayBox
