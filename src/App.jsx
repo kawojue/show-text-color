bc9e27bcbd47ae280da23a03643d5c0c759ff05d
Raheem Kawojue <kawojue08@gmail.com>
2022-10-23 10:41:47 +0100

    initialized toggle display

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,27 @@ import { useState } from 'react'
 import InputColor from "./components/InputColor"
 import DisplayBox from './components/displayBox'
 import ToggleHexValue from './components/ToggleHexValue'
+import ToggleDisplay from './components/ToggleDisplay'
 
 function App() {
   const [inputColor, setInputColor] = useState("")
   const [color, setColor] = useState("")
   const [hexValue, setHexValue] = useState("")
-  const [toggle, setToggle] = useState(false);
+  const [toggle, setToggle] = useState(false)
+  const [displayType, setDisplayType] = useState(true)
 
   const toggleHexValue = () => {
     setToggle(!toggle)
   }
 
+  const toggleDisplay = () => {
+    setDisplayType(!displayType)
+  }
+
   return (
     <>
-      <DisplayBox inputColor={inputColor} color={color} hexValue={hexValue} toggle={toggle} />
+      <ToggleDisplay toggleDisplay={toggleDisplay} displayType={displayType} />
+      <DisplayBox inputColor={inputColor} color={color} hexValue={hexValue} toggle={toggle} displayType={displayType} />
       <InputColor inputColor={inputColor} onSetInputColor={setInputColor} onSetColor={setColor} onSetHexValue={setHexValue} onSetToggle={setToggle} toggle={toggle} />
       <ToggleHexValue toggleHexValue={toggleHexValue} />
     </>
