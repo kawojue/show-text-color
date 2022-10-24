1cc1a4dfbfb665c3926c7092c2cb5498b8b03eff
Raheem Kawojue <kawojue08@gmail.com>
2022-10-24 10:52:53 +0100

    Added info

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import InputColor from "./components/InputColor"
 import DisplayBox from './components/displayBox'
 import ToggleHexValue from './components/ToggleHexValue'
@@ -6,10 +6,11 @@ import ToggleDisplay from './components/ToggleDisplay'
 
 function App() {
   const [inputColor, setInputColor] = useState("")
-  const [color, setColor] = useState("")
-  const [hexValue, setHexValue] = useState("")
   const [toggle, setToggle] = useState(false)
   const [displayType, setDisplayType] = useState(true)
+  const [color, setColor] = useState("")
+  const [info, setInfo] = useState("")
+  const [hexValue, setHexValue] = useState("")
 
   const toggleHexValue = () => {
     setToggle(!toggle)
@@ -19,12 +20,22 @@ function App() {
     setDisplayType(!displayType)
   }
 
+  useEffect(() => {
+    setInfo("Seperate the colors with coma.")
+    setTimeout(() => {
+      setInfo("")
+    }, 4000);
+  }, [displayType])
+
   return (
     <>
-      <ToggleDisplay toggleDisplay={toggleDisplay} displayType={displayType} />
+      {!displayType ? <p>{info}</p> : <p></p>}
+      <div className="flex justify-between my-2">
+        <ToggleDisplay toggleDisplay={toggleDisplay} displayType={displayType} />
+        <ToggleHexValue toggleHexValue={toggleHexValue} />
+      </div>
       <DisplayBox inputColor={inputColor} color={color} hexValue={hexValue} toggle={toggle} displayType={displayType} />
       <InputColor inputColor={inputColor} onSetInputColor={setInputColor} onSetColor={setColor} onSetHexValue={setHexValue} onSetToggle={setToggle} toggle={toggle} />
-      <ToggleHexValue toggleHexValue={toggleHexValue} />
     </>
   )
 }
