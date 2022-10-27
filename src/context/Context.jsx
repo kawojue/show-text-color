ecc2c1cfc67e2bc75d2e972bee73a77964435f41
Raheem Kawojue <kawojue08@gmail.com>
2022-10-27 12:46:25 +0100

    rem info

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -3,7 +3,6 @@ import { createContext, useState, useEffect } from "react";
 const Context = createContext({})
 
 export const DataContext = ({ children }) => {
-    const [info, setInfo] = useState("")
     const [color, setColor] = useState("")
     const [toggle, setToggle] = useState(false)
     const [hexValue, setHexValue] = useState("")
@@ -18,20 +17,13 @@ export const DataContext = ({ children }) => {
         setDisplayType(!displayType)
     }
 
-    useEffect(() => {
-        setInfo("Add color names seperated with commas.")
-        setTimeout(() => {
-            setInfo("")
-        }, 4000);
-    }, [displayType])
-
     return (
         <Context.Provider value={{
             inputColor, setInputColor,
             toggleHexValue, toggleDisplay,
             toggle, setToggle, displayType,
-            setInfo, hexValue, setHexValue,
-            setDisplayType, color, setColor, info,
+            hexValue, setHexValue, color,
+            setColor, setDisplayType,
         }}>
             {children}
         </Context.Provider>
