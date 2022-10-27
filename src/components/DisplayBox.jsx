cecca25e71abccd5dc50e2ef1388ac1ac52fa2e3
Raheem Kawojue <kawojue08@gmail.com>
2022-10-27 12:47:30 +0100

    added Context

--- src/components/DisplayBox.jsx
+++ src/components/DisplayBox.jsx
@@ -1,5 +1,12 @@
-const DisplayBox = ({ inputColor, color, hexValue, toggle, displayType }) => {
+import { useContext } from 'react'
+import Context from '../context/Context'
+
+const DisplayBox = () => {
+    const { inputColor, color, hexValue,
+        toggle, displayType } = useContext(Context)
+
     const formatColor = color.trim().split(',').join(',')
+
     return (
         <div className="displayBox" style={{ background: `${displayType ? color : `linear-gradient(${formatColor})`}` }}>
             <h1 className="text-xl">{inputColor === "" ? "Empty Value" : inputColor}</h1>
