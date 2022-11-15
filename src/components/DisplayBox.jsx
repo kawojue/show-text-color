0f4f11507263be86d39b31d74f7c303453a57032
Raheem Kawojue <kawojue08@gmail.com>
2022-11-15 15:22:39 +0100

    copy btn

--- src/components/DisplayBox.jsx
+++ src/components/DisplayBox.jsx
@@ -2,15 +2,31 @@ import { useContext } from 'react'
 import Context from '../context/Context'
 
 const DisplayBox = () => {
-    const { inputColor, color, hexValue,
-        toggle, displayType } = useContext(Context)
+    const { inputColor, color,
+        hexValue, toggle, displayType,
+        onCopy, copy } = useContext(Context)
 
     const formatColor = color.trim().split(',').join(',')
 
     return (
-        <div className="displayBox" style={{ background: `${displayType ? color : `linear-gradient(${formatColor})`}` }}>
-            <h1 className="text-xl">{inputColor === "" ? "Empty Value" : inputColor}</h1>
-            {toggle && <h1 className="text-lg">{hexValue}</h1>}
+        <div className="displayBox" style={{
+            background:
+                `${displayType ? color :
+                    `linear-gradient(${formatColor})`}`
+        }}>
+            <h1 className="text-xl">
+                {inputColor === '' ? 'Empty Value' : inputColor}
+            </h1>
+            {toggle &&
+                <div className="hex-container">
+                    <h1 className="text-lg">
+                        {hexValue}
+                    </h1>
+                    <button onClick={() => onCopy(hexValue)}>
+                        {copy}
+                    </button>
+                </div>
+            }
         </div>
     )
 }
