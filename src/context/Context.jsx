4dcc2c6318d78476a059513e1d5ef4d33ef39680
Raheem Kawojue <kawojue08@gmail.com>
2022-11-15 15:23:03 +0100

    copy func

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,4 +1,5 @@
-import { createContext, useState, useEffect } from "react";
+import { FaCopy } from 'react-icons/fa'
+import { createContext, useState } from "react";
 
 const Context = createContext({})
 
@@ -6,6 +7,7 @@ export const DataContext = ({ children }) => {
     const [color, setColor] = useState("")
     const [toggle, setToggle] = useState(false)
     const [hexValue, setHexValue] = useState("")
+    const [copy, setCopy] = useState(<FaCopy />)
     const [inputColor, setInputColor] = useState("")
     const [displayType, setDisplayType] = useState(true)
 
@@ -17,13 +19,25 @@ export const DataContext = ({ children }) => {
         setDisplayType(!displayType)
     }
 
+    const onCopy = async value => {
+        try {
+            await navigator.clipboard.writeText(value)
+            setCopy("Copied")
+            setTimeout(() => {
+                setCopy(<FaCopy />)
+            }, 1000)
+        } catch (err) {
+            setCopy("Failed!")
+        }
+    }
+
     return (
         <Context.Provider value={{
             inputColor, setInputColor,
             toggleHexValue, toggleDisplay,
             toggle, setToggle, displayType,
             hexValue, setHexValue, color,
-            setColor, setDisplayType,
+            setColor, setDisplayType, onCopy, copy
         }}>
             {children}
         </Context.Provider>
